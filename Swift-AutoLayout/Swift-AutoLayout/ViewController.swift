e296e519ceabc5717c409464c9160666c8e5e641
KerryDeng <dkts47@163.com>
2016-04-02 12:50:45 +0800

    添加Swift-AutoLayout

---
+++ Swift-AutoLayout/Swift-AutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-AutoLayout
+//
+//  Created by Kerry on 16/3/13.
+//  Copyright © 2016年 DKT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
